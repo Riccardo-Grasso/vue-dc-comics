c16cfd2625c1b30e003858246445a3721ae58b8f
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-11-11 14:43:42 +0100

    nav

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,12 +1,65 @@
 <template>
   <header>
     <img src="../assets/dc-logo.png" alt="DC_Logo" />
+    <div class="navbar">
+      <ul>
+        <li class="navbar-item" v-for="(item, i) in navbarLinks" :key="i">
+          <a href="item.href">{{ item.title }}</a>
+        </li>
+      </ul>
+    </div>
   </header>
 </template>
 
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      navbarLinks: [
+        {
+          title: "CHARACTERS",
+          href: "#",
+        },
+        {
+          title: "COMICS",
+          href: "#",
+        },
+        {
+          title: "MOVIES",
+          href: "#",
+        },
+        {
+          title: "TV",
+          href: "#",
+        },
+        {
+          title: "GAMES",
+          href: "#",
+        },
+        {
+          title: "COLLECTIBLES",
+          href: "#",
+        },
+        {
+          title: "VIDEOS",
+          href: "#",
+        },
+        {
+          title: "FANS",
+          href: "#",
+        },
+        {
+          title: "NEWS",
+          href: "#",
+        },
+        {
+          title: "SHOP",
+          href: "#",
+        },
+      ],
+    };
+  },
 };
 </script>
 
