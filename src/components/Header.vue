1814d97f929dcd8f2d17271de16428eb32236f30
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-11-11 14:18:48 +0100

    start

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <img src="../assets/dc-logo.png" alt="DC_Logo" />
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style scoped>
+</style>
