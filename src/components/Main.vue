f55290b83bf8011e5f0a7db7dee0a00a2538364f
Riccardo-Grasso <riccardo.grasso@outlook.it>
2021-11-12 17:50:29 +0100

    elements

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,7 +3,10 @@
     <div class="jumbotron">
       <img src="@/assets/jumbotron.jpg" alt="jumbo-img" />
     </div>
+
     <div class="comics-container">
+      <div class="current-series">CURRENT SERIES</div>
+
       <Products
         v-for="(p, i) in comicsList"
         :key="i"
@@ -12,6 +15,12 @@
         :price="p.price"
         :type="p.type"
       ></Products>
+    </div>
+
+    <div class="row">
+      <div class="load-more">
+        <a href="#">LOAD MORE</a>
+      </div>
     </div>
   </main>
 </template>
